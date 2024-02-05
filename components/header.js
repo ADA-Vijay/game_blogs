1fa7c386dff9ad222f5df4648e4da9c839eec660
vijay saroj <vijaysaroj4527@gmail.com>
2024-02-05 19:37:08 +0530

    initial changes

---
+++ components/header.js
@@ -0,0 +1,47 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import styles from "@/styles/Header.module.css";
+
+function Header() {
+  return (
+    <Navbar expand="lg" className={styles.headerWrap}>
+      <Container>
+        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+        <Navbar.Brand href="#home">Gamewitted</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home" className={styles.headerlink}>Home</Nav.Link>
+            <NavDropdown title="Gaming" className={styles.headerDropdown}>
+              <NavDropdown.Item href="#action/3.1">News</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Guides</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#link" className={styles.headerlink}>Anime</Nav.Link>
+            <div className={styles.extraHeaderWrap}>
+                <div className={styles.searchHeaderWrap}>
+                    <div className={styles.searchInput}>
+                        <Form.Control type="text" placeholder="" />
+                        <span className={styles.searchIcon}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                            </svg>
+                        </span>
+                    </div>
+                </div>
+                <div className={styles.headerMenuWrap}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                    </svg>
+                </div>
+            </div>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
---
+++ pages/[blogs]/[sub_blogs]/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <dv>sub blogs</dv>
+  )
+}
+
+export default index
