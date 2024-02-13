d037b52444403c0fddc1fff068b30795e4d9017f
Farhank19 <farhankazi887@gmail.com>
2024-02-13 17:03:18 +0530

    .env.local changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env :{
+    NEXT_PUBLIC_API_URL:"https://ashgamewitted.wpcomstaging.com/wp-json/wp/v2/"
+
+  }
+  
 };
 
 export default nextConfig;
