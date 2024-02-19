516376ffdedb27ac056740e6b55de5db35f98695
vijay4527 <vijaysaroj4527@gmail.com>
2024-02-19 19:46:54 +0530

    new changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import "@/styles/globals.css";
-import Header from "@/components/header"
-
+import Header from "@/components/header";
+import { DefaultSeo } from "next-seo";
+import SEOConfig from '../next-seo.config';
 export default function App({ Component, pageProps }) {
   return (
     <>
-        <Header />
-        <Component {...pageProps} />
-
+      <DefaultSeo {...SEOConfig} />
+       <Header />
+      <Component {...pageProps} />
     </>
-  )
+  );
 }
